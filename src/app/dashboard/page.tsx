e01c583a82c647e962b93e77fa8dd61996188f35
Sinaver <sinaver.osmanov@gmail.com>
2023-06-02 13:40:28 +0200

    chapter1

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { FC } from "react";
+
+export interface IPageProps {}
+
+const page: FC<IPageProps> = ({}) => {
+	return <Button>Button</Button>;
+};
+
+export default page;
