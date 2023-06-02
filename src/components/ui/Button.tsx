78251296bce5ffd563e8e3c32c8b553e7ad0d821
Sinaver <sinaver.osmanov@gmail.com>
2023-06-02 15:28:25 +0200

    chapter2-auth

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import { Loader2 } from "lucide-react";
 import { ButtonHTMLAttributes, FC } from "react";
+
 const buttonVariance = cva(
 	"active:scale-95 inline-flex items-center justify-center rouded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disable:pointer-events-none",
 	{
